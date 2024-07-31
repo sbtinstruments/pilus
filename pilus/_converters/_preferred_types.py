b2142d3126895ab8ed66ace477c4c2f34706705d
Frederik Aalund <fpa@sbtinstruments.com>
2024-07-31 13:07:31 +0000

    refactor: remove unused file

--- pilus/_converters/_preferred_types.py
+++
@@ -1,9 +0,0 @@
-from typing import Any
-
-from ..basic import Lpcm
-from ..snipdb import SnipDb
-
-# Mapping of media type to type.
-# We use this mapping when we, e.g., deserialize files from the box/snip
-# container format.
-PREFERRED_TYPES: dict[str, Any] = {}

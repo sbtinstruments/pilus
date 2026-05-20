5fb62fd72c0d14d052787015f8d3a0dc0e51aa66
Frederik Aalund <fpa@sbtinstruments.com>
2026-05-20 20:19:12 +0000

    feat: make lago_cache commit_id specific

--- tests/_assets.py
+++ tests/_assets.py
@@ -1,5 +1,8 @@
 from lago.low_level import setup_and_get_assets_dir
 
+_PUBLIC_ASSETS_GIT_URL = "https://github.com/sbtinstruments/assets.git"
+_PUBLIC_ASSETS_COMMIT = "7ebc2a5711eaa7c36e5effc265b82cfc10cc64e9"
+
 PUBLIC_ASSETS_DIR = setup_and_get_assets_dir(
-    git_url="https://github.com/sbtinstruments/assets.git"
+    git_url=_PUBLIC_ASSETS_GIT_URL, commit_id=_PUBLIC_ASSETS_COMMIT
 )
